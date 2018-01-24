6baac0885dc8dcace2e5710ad140400a5b627228
Dérick Hogan Pimenta <derickhoganpimenta@yahoo.com.br>
2018-01-24 14:43:46 -0200

    Change jcrop definitions

--- vendor/assets/javascripts/rails_admin/jquery.Jcrop.js
+++ vendor/assets/javascripts/rails_admin/jquery.Jcrop.js
@@ -1,6 +1,6 @@
 /**
  * jquery.Jcrop.js v0.9.12
- * jQuery Image Cropping Plugin - released under MIT License 
+ * jQuery Image Cropping Plugin - released under MIT License
  * Author: Kelly Hallman <khallman@gmail.com>
  * http://github.com/tapmodo/Jcrop
  * Copyright (c) 2008-2013 Tapmodo Interactive LLC {{{
@@ -171,7 +171,7 @@
         if ((ord === 'move') && !options.allowMove) {
           return false;
         }
-        
+
         // Fix position of crop area when dragged the very first time.
         // Necessary when crop image is in a hidden element when page is loaded.
         docOffset = getPos($img);
@@ -304,12 +304,12 @@
         $origimg.width($origimg[0].width);
         $origimg.height($origimg[0].height);
       } else {
-        // Obtain dimensions from temporary image in case the original is not loaded yet (e.g. IE 7.0). 
+        // Obtain dimensions from temporary image in case the original is not loaded yet (e.g. IE 7.0).
         var tempImage = new Image();
         tempImage.src = $origimg[0].src;
         $origimg.width(tempImage.width);
         $origimg.height(tempImage.height);
-      } 
+      }
 
       var $img = $origimg.clone().removeAttr('id').css(img_css).show();
 
@@ -327,8 +327,8 @@
 
     var boundx = $img.width(),
         boundy = $img.height(),
-        
-        
+
+
         $div = $('<div />').width(boundx).height(boundy).addClass(cssClass('holder')).css({
         position: 'relative',
         backgroundColor: options.bgColor
@@ -340,24 +340,24 @@
 
     var $img2 = $('<div />'),
 
-        $img_holder = $('<div />') 
+        $img_holder = $('<div />')
         .width('100%').height('100%').css({
           zIndex: 310,
           position: 'absolute',
           overflow: 'hidden'
         }),
 
-        $hdl_holder = $('<div />') 
-        .width('100%').height('100%').css('zIndex', 320), 
+        $hdl_holder = $('<div />')
+        .width('100%').height('100%').css('zIndex', 320),
 
-        $sel = $('<div />') 
+        $sel = $('<div />')
         .css({
           position: 'absolute',
           zIndex: 600
         }).dblclick(function(){
           var c = Coords.getFixed();
           options.onDblClick.call(api,c);
-        }).insertBefore($img).append($img_holder, $hdl_holder); 
+        }).insertBefore($img).append($img_holder, $hdl_holder);
 
     if (img_mode) {
 
@@ -393,7 +393,7 @@
     // }}}
     // }}}
     // Internal Modules {{{
-    // Touch Module {{{ 
+    // Touch Module {{{
     var Touch = (function () {
       // Touch support detection function adapted (under MIT License)
       // from code by Jeffrey Sambells - http://github.com/iamamused/
@@ -530,8 +530,8 @@
         // This function could use some optimization I think...
         var aspect = options.aspectRatio,
             min_x = options.minSize[0] / xscale,
-            
-            
+
+
             //min_y = options.minSize[1]/yscale,
             max_x = options.maxSize[0] / xscale,
             max_y = options.maxSize[1] / yscale,
@@ -1047,7 +1047,7 @@
       {
         seehandles = false;
         $hdl_holder.hide();
-      } 
+      }
       //}}}
       function animMode(v) //{{{
       {
@@ -1058,13 +1058,13 @@
           animating = false;
           enableHandles();
         }
-      } 
+      }
       //}}}
       function done() //{{{
       {
         animMode(false);
         refresh();
-      } 
+      }
       //}}}
       // Insert draggable elements {{{
       // Insert border divs for outline
@@ -1120,7 +1120,7 @@
         done: done
       };
     }());
-    
+
     //}}}
     // Tracker Module {{{
     var Tracker = (function () {
@@ -1143,7 +1143,7 @@
           $(document)
             .bind('mousemove.jcrop',trackMove)
             .bind('mouseup.jcrop',trackUp);
-      } 
+      }
       //}}}
       function toBack() //{{{
       {
@@ -1156,13 +1156,13 @@
         $(document).unbind('touchend.jcrop');
         $(document).unbind('mousemove.jcrop');
         $(document).unbind('mouseup.jcrop');
-      } 
+      }
       //}}}
       function trackMove(e) //{{{
       {
         onMove(mouseAbs(e));
         return false;
-      } 
+      }
       //}}}
       function trackUp(e) //{{{
       {
@@ -1547,7 +1547,7 @@
     var api = {
       setImage: setImage,
       animateTo: animateTo,
-      setSelect: setSelect,
+      setSelect: [0, 0, 255, 194],
       setOptions: setOptionsNew,
       tellSelect: tellSelect,
       tellScaled: tellScaled,
@@ -1651,7 +1651,7 @@
     // Basic Settings
     allowSelect: true,
     allowMove: true,
-    allowResize: true,
+    allowResize: false,
 
     trackDocument: true,
 
@@ -1665,7 +1665,8 @@
     handleOpacity: 0.5,
     handleSize: null,
 
-    aspectRatio: 0,
+    aspectRatio: 1,
+    setSelect: [0, 0, 255, 194],
     keySupport: true,
     createHandles: ['n','s','e','w','nw','ne','se','sw'],
     createDragbars: ['n','s','e','w'],
@@ -1685,8 +1686,8 @@
     swingSpeed: 3,
 
     minSelect: [0, 0],
-    maxSize: [0, 0],
-    minSize: [0, 0],
+    maxSize: [255, 194],
+    minSize: [255, 194],
 
     // Callbacks / Event Handlers
     onChange: function () {},
